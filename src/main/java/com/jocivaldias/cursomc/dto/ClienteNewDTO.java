f17bec337bc66b279c961897f0eb168637c1f4f8
Jocival Dias <jocival.dias@hotmail.com>
2020-10-05 19:47:27 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/jocivaldias/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/jocivaldias/cursomc/dto/ClienteNewDTO.java
@@ -1,21 +1,40 @@
 package com.jocivaldias.cursomc.dto;
 
+import com.jocivaldias.cursomc.services.validation.ClienteInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.EmbeddedId;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    //Dados cliente
+    @NotBlank(message = "Preenchimento obrigatório.")
+    @Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres.")
     private String nome;
+    @NotBlank(message = "Preenchimento obrigatório.")
+    @Email(message = "E-mail inválido")
     private String email;
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String cpfOuCnpj;
     private Integer tipo;
 
+    //Dados endereço
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String logradouro;
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String numero;
     private String complemento;
     private String bairro;
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String cep;
 
+    //Dados telefone
+    @NotBlank(message = "Preenchimento obrigatório.")
     private String telefone1;
     private String telefone2;
     private String telefone3;
